4a2e2f97aa15608ce011a638bf6363b9c5ecb0b6
Cameron <cwbarclift@gmail.com>
2017-07-24 23:25:20 -0400

    better json handling

--- src/main/scala/com/cliftbar/akkadisaster/AkkaDisasterApp.scala
+++ src/main/scala/com/cliftbar/akkadisaster/AkkaDisasterApp.scala
@@ -27,9 +27,10 @@ object AkkaDisasterApp extends HttpApp with App {
         complete(j.toJson.toString) // Completes with some text
       }
     } ~
-    path("jsonIn") { // Path to read and print json.  It's a little round about.  Test with {"item": "value"}
+    path("calculateHurricane") { // Path to read and print json.  It's a little round about.  Test with {"item": "value"}
       get {
         entity(as[String]) { json =>
+          println(Some("NaN".toDouble))
           val parsed = JsonParser(json).asJsObject
           println(json)
 
@@ -49,86 +50,108 @@ object AkkaDisasterApp extends HttpApp with App {
 
           val track = parsed.fields("track")
 
-          object TrackPointJsonProtocol extends DefaultJsonProtocol {
+          object TrackPointJsonProtocol extends DefaultJsonProtocol with NullOptions {
             implicit object TrackPointJsonFormat extends RootJsonFormat[nws.TrackPoint] {
               def write(tp: nws.TrackPoint) =
                 JsArray(JsString(tp.stormName), JsNumber(tp.rMax_nmi), JsNumber(tp.fSpeed_kts), JsNumber(tp.gwaf))
-              def read(value: JsValue) = {
-                value.asJsObject.getFields(
-                  "catalogNumber"
-                  ,"stormName"
-                  ,"basin"
-                  ,"timestamp"
-                  ,"eyeLat_y"
-                  ,"eyeLon_x"
-                  ,"maxWind_kts"
-                  ,"minCp_mb"
-                  ,"sequence"
-                  ,"fSpeed_kts"
-                  ,"isLandfallPoint"
-                  ,"rMax_nmi"
-                  ,"gwaf"
-                  ,"heading"
-                ) match {
-                  case Seq(
-                    JsNumber(catalogNumber)
-                    ,JsString(stormName)
-                    ,JsString(basin)
-                    , JsString(timestamp)
-                    , JsNumber(eyeLat_y)
-                    , JsNumber(eyeLon_x)
-                    , JsNumber(maxWind_kts)
-                    , JsNumber(minCp_mb)
-                    , JsNumber(sequence)
-                    , JsNumber(fSpeed_kts)
-                    , JsBoolean(isLandfallPoint)
-                    , JsNumber(rMax_nmi)
-                    , JsNumber(gwaf)
-                    , JsNumber(heading)
-                  ) =>
-                      new nws.TrackPoint(
-                      Some(catalogNumber.toInt)
-                      ,stormName
-                      ,Some(basin)
-                      ,LocalDateTime.parse(timestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
-                      ,eyeLat_y.toDouble
-                      ,eyeLon_x.toDouble
-                      ,Some(maxWind_kts.toDouble)
-                      ,Some(minCp_mb.toDouble)
-                      ,sequence.toDouble
-                      ,fSpeed_kts.toDouble
-                      ,isLandfallPoint
-                      ,rMax_nmi.toDouble
-                      ,gwaf.toDouble
-                      ,Some(heading.toDouble)
+              def read (value: JsValue) = value match {
+                case JsObject(fields) =>
+                  println(fields)
+                  val catalogNumber: Option[Int] = fields.get("catalogNumber").map(x => x.convertTo[Int]) //fields.get("catalogNumber").asInstanceOf[Some[Int]]
+                  val stormName: String = fields("stormName").convertTo[String]
+                  val basin: Option[String] = fields.get("basin").map(x => x.convertTo[String])
+                  val timestamp: String = fields("timestamp").convertTo[String]
+                  val eyeLat_y: Double = fields("eyeLat_y").convertTo[Double]
+                  val eyeLon_x: Double = fields("eyeLon_x").convertTo[Double]
+                  val maxWind_kts: Option[Double] = fields.get("maxWind_kts").map(x => x.convertTo[Double]) //fields.get("maxWind_kts").asInstanceOf[Some[Double]].orElse(None)
+                  val minCp_mb: Option[Double] = fields.get("minCp_mb").map(x => x.convertTo[Double]) //fields.get("minCp_mb").asInstanceOf[Some[Double]].orElse(None)
+                  val sequence: Double = fields("sequence").convertTo[Double]
+                  val fSpeed_kts: Double = fields("fSpeed_kts").convertTo[Double]
+                  val isLandfallPoint: Boolean = fields("isLandfallPoint").convertTo[Boolean]
+                  val rMax_nmi: Double = fields("rMax_nmi").convertTo[Double]
+                  val gwaf: Double = fields("gwaf").convertTo[Double]
+                  val heading: Option[Double] = fields.get("heading").map(x => x.convertTo[Double]) //fields.get("heading").asInstanceOf[Some[Double]].orElse(None)
+                  new nws.TrackPoint(
+                    catalogNumber
+                    ,stormName
+                    ,basin
+                    ,LocalDateTime.parse(timestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm")) //LocalDateTime.now()
+                    ,eyeLat_y
+                    ,eyeLon_x
+                    ,maxWind_kts
+                    ,minCp_mb
+                    ,sequence
+                    ,fSpeed_kts
+                    ,isLandfallPoint
+                    ,rMax_nmi
+                    ,gwaf
+                    ,heading
                   )
-                  case _ => throw new DeserializationException("Expected Track Point")
-                }
+//              def read(value: JsValue) = {
+//                value.asJsObject.getFields(
+//                  "catalogNumber"
+//                  ,"stormName"
+//                  ,"basin"
+//                  ,"timestamp"
+//                  ,"eyeLat_y"
+//                  ,"eyeLon_x"
+//                  ,"maxWind_kts"
+//                  ,"minCp_mb"
+//                  ,"sequence"
+//                  ,"fSpeed_kts"
+//                  ,"isLandfallPoint"
+//                  ,"rMax_nmi"
+//                  ,"gwaf"
+//                  ,"heading"
+//                ) match {
+//                  case Seq(
+//                    JsNumber(catalogNumber)
+//                    ,JsString(stormName)
+//                    ,JsString(basin)
+//                    , JsString(timestamp)
+//                    , JsNumber(eyeLat_y)
+//                    , JsNumber(eyeLon_x)
+//                    , JsNumber(maxWind_kts)
+//                    , JsNull
+//                    , JsNumber(sequence)
+//                    , JsNumber(fSpeed_kts)
+//                    , JsBoolean(isLandfallPoint)
+//                    , JsNumber(rMax_nmi)
+//                    , JsNumber(gwaf)
+//                    , JsNumber(heading)
+//                  ) =>
+//                      new nws.TrackPoint(
+//                      Some(catalogNumber.toInt)
+//                      ,stormName
+//                      ,Some(basin)
+//                      ,LocalDateTime.parse(timestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
+//                      ,eyeLat_y.toDouble
+//                      ,eyeLon_x.toDouble
+//                      ,Some(maxWind_kts.toDouble)
+//                      ,Some(0) //minCp_mb.contains()
+//                      ,sequence.toDouble
+//                      ,fSpeed_kts.toDouble
+//                      ,isLandfallPoint
+//                      ,rMax_nmi.toDouble
+//                      ,gwaf.toDouble
+//                      ,Some(heading.toDouble)
+//                  )
+//                  case _ => throw new DeserializationException(value.asJsObject.getFields("minCp_mb")(0))
+//                }
               }
             }
           }
 
           import TrackPointJsonProtocol._
-          val parsedTrack = track.convertTo[Seq[nws.TrackPoint]]
+          val parsedTrack = track.convertTo[List[nws.TrackPoint]]
           //val trackList = track
 //
-          println(parsedTrack(0).stormName)
+          println(parsedTrack(0).catalogNumber.get)
+          val str = parsedTrack(0).basin.get
+          println(str)
 
           println(maxDist)
-//          val catalogNumber : scala.Option[scala.Int]
-//          val stormName : _root_.scala.Predef.String
-//          val basin : scala.Option[_root_.scala.Predef.String]
-//          val timestamp : java.time.LocalDateTime
-//          val eyeLat_y : scala.Double
-//          val eyeLon_x : scala.Double
-//          val maxWind_kts : scala.Option[scala.Double]
-//          val minCp_mb : scala.Option[scala.Double]
-//          val sequence : scala.Double
-//          val fSpeed_kts : scala.Double
-//          val isLandfallPoint : scala.Boolean
-//          val rMax_nmi : scala.Double
-//          val gwaf : scala.Double
-//          val heading : scala.Option[scala.Double]
+
           complete("read json") // Completes with some text
         }
       }
